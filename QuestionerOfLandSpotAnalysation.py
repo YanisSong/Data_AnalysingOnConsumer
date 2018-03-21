7099551d081d9e21f1c79f5c97f79aa9e6702712
Sioa <iamsioa@163.com>
2018-03-21 20:18:15 +0800

    Function design.

--- QuestionerOfLandSpotAnalysation.py
+++ QuestionerOfLandSpotAnalysation.py
@@ -1,30 +1,55 @@
+# This module is used to analysing the questioner that is collected from the land spots.
+import re
 
+
+# data input and output interface.
 def dataProgramming(cf, rf):
     filename = "D:\workDir\客服部调查问卷分析报告\云南5A风景区电信网络质量调查问卷清单.csv"
-    dsename = "D:\workDir\客服部调查问卷分析报告\landspotResult.txt"
-    # with open(rf, "a") as head:
-    #     head.write("eNBId, objectId, LteScRSRP, LteNcRSRP, LteScRSRQ, LteNcRSRQ, LteScTadv, "
-    #                "LteScAOA, LteScSinrUL, LteScEarfcn, LteScPci, Longitude, Latitude" + "\n")
-    # head.close()
+    outputUrl = "D:\workDir\客服部调查问卷分析报告\landspotResult.txt"
+    countingDict = {}
+    suggesstionDick = {}
+    # Add userDict in the future, which is used to analysing user information.
     fr = open(filename, encoding='gb18030', errors='ignore')
     for line in fr:
-        # reason = processingLine(line)
-        with open(dsename, "a") as destiny:
-            destiny.write(line + '\n')
-    destiny.close()
+        reason = processingLine(line, outputUrl)
+        if reason:
+            analysingProcess(reason, countingDict, suggesstionDick)
     fr.close()
 
 
-def processingLine(value):
+#analysding function
+def analysingProcess(reason, countingDict, suggesstionDick):
+    if reason[0] != "---" and reason[0] != "上述一个都没去过":
+        if reason[1].find("满意") or reason[1].find("惊喜"):
+            # Do nothing
+        else:
+            if reason[0] in countingDict:
+                countingDict[reason[0]] += 1
+            else:
+                countingDict += {reason[0] : 1}
+        suggesstion = "满意度调查--"+"通话："+ reason[3] + "即时信息:" + reason[4] + "" + "" + ""
+        suggesstionDick[]
+
+
+#data cleaning function.
+def processingLine(value, outputUrl):
+    involvingNumber = re.findall(r"\d{11}", value)
+    if not involvingNumber:
+        return
     valueSet = value.split(",")
     valueList = []
-    markSet = [31, 36, 4, 23, 5, 24, 6, 8, 9, 2, 3, 19, 20]
-    for i in range(0, 13):
-        if valueSet[markSet[i] - 1] == " ''":
-            valueSet[markSet[i] - 1] = " '---'"
+    markSet = [7, 8, 19, 20, 23, 24, 30]
+    for i in range(0, 7):
+        if len(valueSet) < 30:
+            continue
+        if valueSet[markSet[i] - 1] == "":
+            valueSet[markSet[i] - 1] = "---"
         valueList.append(valueSet[markSet[i] - 1])
-    valueList.append(' --Ending;\n')
     reasonItem = ''.join(valueList)
-    return reasonItem
+    with open(outputUrl, "a") as destiny:
+        destiny.write(reasonItem)
+    return valueList
 
-dataProgramming(" ", " ")
+
+# @Test
+# dataProgramming(" ", " ")
